e373f4a4d0b608289e022c17198a08950ff7e718
andriussvilys <iciukas@gmail.com>
2020-08-27 02:32:36 +0300

    Updates

---
+++ precache-manifest.7873c61aab2a3cac9e9a1491230fe585.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15963ba0b350565557b9d047c46181a8",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "09e7b02df02c31b8e324",
+    "url": "/landing-page/static/css/2.f835e8e5.chunk.css"
+  },
+  {
+    "revision": "b783755ac7cfb2ec445f",
+    "url": "/landing-page/static/css/main.4ddc9e33.chunk.css"
+  },
+  {
+    "revision": "09e7b02df02c31b8e324",
+    "url": "/landing-page/static/js/2.65865d31.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/landing-page/static/js/2.65865d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b783755ac7cfb2ec445f",
+    "url": "/landing-page/static/js/main.47174aa5.chunk.js"
+  },
+  {
+    "revision": "d803e7ff0721cd373432",
+    "url": "/landing-page/static/js/runtime-main.8c4e35fe.js"
+  },
+  {
+    "revision": "d68bdb901e6c6e41e6074c156906cc83",
+    "url": "/landing-page/static/media/arrow-2.d68bdb90.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/landing-page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/landing-page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/landing-page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/landing-page/static/media/slick.f97e3bbf.svg"
+  }
+]);
