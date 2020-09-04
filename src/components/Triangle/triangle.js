7615f66466662ad487682d9df650fe1f95c30fda
Andrius Svilys <iciukas@gmail.com>
2020-09-04 15:31:29 +0300

    triangle labels

--- src/components/Triangle/triangle.js
+++ src/components/Triangle/triangle.js
@@ -3,26 +3,35 @@ import './css/alt-css/triangle.css'
 
 const Triangle = () => {
     return <div className="spinner-container">
-                    <div className="circle-labels">
+                    {/* <div className="circle-labels">
                             <span className="circle-label circle-label_1">IMAGINE</span>
                             <span className="circle-label circle-label_2">PLAN</span>
                             <span className="circle-label circle-label_3">BUILD</span>
-                    </div>
+                    </div> */}
                     <div className="circleBound">
                         <div className="innerCircle">
                             <div className="triangle-wrapper">
                                 <div className="circle circle-1">
                                     <div className="circle-inner circle-inner_1">
+                                        <div className="circle-label-wrapper">
+                                            <span className="circle-label circle-label_1">IMAGINE</span>
+                                        </div>
                                         <img src="triangle/lightbulb-emoji.png" alt="lightbulb emoji" />
                                     </div>
                                 </div>
                                 <div className="circle circle-2">
                                     <div className="circle-inner circle-inner_2">
+                                        <div className="circle-label-wrapper">
+                                            <span className="circle-label circle-label_2">BUILD</span>
+                                        </div>
                                         <img src="triangle/write-emoji.png" alt="write emoji" />
                                     </div>
                                 </div>
                                 <div className="circle circle-3">
                                     <div className="circle-inner circle-inner_3">
+                                        <div className="circle-label-wrapper">
+                                            <span className="circle-label circle-label_3">PLAN</span>
+                                        </div>
                                         <img src="triangle/plan-emoji.png" alt="map emoji" />
                                     </div>
                                 </div>
